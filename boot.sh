aaa88a6b388cff5885146a2ee51ecec0c035392f
Peter Senna Tschudin <peter.senna@gmail.com>
2018-04-15 11:07:39 +0200

    Workaround ENV vars not being propagated to client
    When OpenVPN calls a script it does not pass existing environment
    variables. This workaround creates a json file from environment
    variables that the script loads.
    
    Signed-off-by: Peter Senna Tschudin <peter.senna@gmail.com>
    Signed-off-by: Peter Senna Tschudin <peter.senna@iwe.io>

--- boot.sh
+++ boot.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+if [[ "$CLIENT_CONNECT_URL" == "" ]];then
+	echo "Please define CLIENT_CONNECT_URL"
+	exit
+fi
+
+if [[ "$CLIENT_CONNECT_SECRET" == "" ]];then
+	echo "Please define CLIENT_CONNECT_SECRET"
+	exit
+fi
+
+echo "{\"client_connect_url\": \"$CLIENT_CONNECT_URL\", \"client_connect_secret\": \"$CLIENT_CONNECT_SECRET\"}" > /etc/client_connect.json
+
 mkdir -p /dev/net
 if [ ! -c /dev/net/tun ]; then
 	mknod /dev/net/tun c 10 200
--- docker/start.sh
+++ docker/start.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-VPN_CLIENT_CONNECT_SECRET_KEY=BF9guWTBFEyrUKX2Go6twtQt
+CLIENT_CONNECT_URL="https://beta.iwe.cloud/backend/vpn/hosts/client-connect/"
+CLIENT_CONNECT_SECRET="BF9guWTBFEyrUKX2Go6twtQt"
 
 docker run \
 	-d \
 	--name iwe_hosts_vpn \
 	--restart unless-stopped \
-	-e VPN_CLIENT_CONNECT_SECRET_KEY=$VPN_CLIENT_CONNECT_SECRET_KEY \
+	-e CLIENT_CONNECT_URL=$CLIENT_CONNECT_URL \
+	-e CLIENT_CONNECT_SECRET=$CLIENT_CONNECT_SECRET \
 	-p 1194:1194/udp \
 	--cap-add=NET_ADMIN \
 	iwe_hosts_vpn:latest
