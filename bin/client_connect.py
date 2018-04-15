c068bf6719468dd38f4ce1b62586346350e2d0de
Peter Senna Tschudin <peter.senna@gmail.com>
2018-04-15 11:10:37 +0200

    client_connect.py: move from ENV vars to json file
    Get URL and secret from a json file instead of from environment
    variables.
    
    Signed-off-by: Peter Senna Tschudin <peter.senna@gmail.com>
    Signed-off-by: Peter Senna Tschudin <peter.senna@iwe.io>

--- bin/client_connect.py
+++ bin/client_connect.py
@@ -1,16 +1,11 @@
-#!/usr/bin/env python3
+#!/etc/openvpn/bin/venv/bin/python3
 ''' Check if client with this CommonName should be allowed to connect '''
 
 import errno
 import os
 import sys
 import requests
-
-VPN_CLIENT_CONNECT_URL = os.environ.get('VPN_CLIENT_CONNECT_URL') \
-    or 'https://beta.iwe.cloud/backend/vpn/hosts/client-connect'
-
-VPN_CLIENT_CONNECT_SECRET_KEY = os.environ.get('VPN_CLIENT_CONNECT_SECRET_KEY') \
-    or 'Su6oLpYQv5whthmNrjtf'
+import simplejson as json
 
 def main():
     ''' good old main '''
@@ -18,27 +13,44 @@ def main():
     environ = os.environ
     if not 'common_name' in environ.keys():
         print('Where are the environment variables?')
-        sys.exit(-errno.EACCES)
+        sys.exit(errno.EACCES)
+
+    conf = json.load(open('/etc/client_connect.json'))
+    if not conf:
+        print('Where is the configuration file?')
+        sys.exit(errno.EACCES)
+
+    client_connect_url = conf['client_connect_url']
+    client_connect_secret = conf['client_connect_secret']
 
     common_name = environ['common_name']
+    envdict = {}
+    for key in environ.keys():
+        envdict[key] = environ[key]
 
     try:
         client_connect = requests.post(
-            '{}/{}'.format(VPN_CLIENT_CONNECT_URL, common_name),
-            headers={'secret-key': VPN_CLIENT_CONNECT_SECRET_KEY},
-            data=environ)
+            '{}/{}'.format(client_connect_url, common_name),
+            headers={'secret-key': client_connect_secret},
+            json=envdict)
     except:
-        print('Something wrong when asking iWegard about {}'.format(common_name))
-        sys.exit(-errno.EACCES)
+        print('Something wrong when asking iWegard about {}. Got an exception on requests.post(...)'.format(common_name))
+        sys.exit(errno.EACCES)
+
+    if client_connect.status_code != 200:
+        print('Something wrong when asking iWegard about {}. status_code = {}'.format(common_name, client_connect.status_code))
+        sys.exit(errno.EACCES)
 
     client_connect_json = client_connect.json()
 
     if client_connect_json['uuid'] == common_name \
        and client_connect_json['vpn-name'] == 'hosts' \
        and client_connect_json['allow-connection'] == 'True':
+        print('iWegard says that {} is good to connect'.format(common_name))
         sys.exit(0)
 
-    sys.exit(-errno.EPERM)
+    print('iWegard says that {} is NOT good to connect'.format(common_name))
+    sys.exit(errno.EPERM)
 
 
 if __name__ == "__main__":
